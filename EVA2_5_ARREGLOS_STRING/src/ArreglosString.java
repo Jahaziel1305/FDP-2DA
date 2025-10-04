4c8affe2c0e3d62a44cd672ed53a5d5fa10ccb9b
Jahaziel <l25550366@chihuahua2.tecnm.mx>
2025-10-04 17:56:07 -0600

    #

---
+++ EVA2_5_ARREGLOS_STRING/src/ArreglosString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArreglosString {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int dia;
+        String diasSemana[] = new String[7];
+        diasSemana[0] = "Domingo";
+        diasSemana[1] = "Lunes";
+        diasSemana[2] = "Martes";
+        diasSemana[3] = "Miercoles";
+        diasSemana[4] = "Jueves";
+        diasSemana[5] = "Viernes";
+        diasSemana[6] = "Sabado";
+        System.out.println("Introduce el dia en numero (0-6): ");
+        dia = teclado.nextInt();
+        System.out.println("El dia es :" + diasSemana[dia]);
+    }
+}
