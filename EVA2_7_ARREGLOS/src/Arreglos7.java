32869ab6336c2185ed96cb368a2b23efcf333e10
Jahaziel <l25550366@chihuahua2.tecnm.mx>
2025-10-09 12:10:56 -0600

    #

---
+++ EVA2_7_ARREGLOS/src/Arreglos7.java
@@ -0,0 +1,9 @@
+public class Arreglos7 {
+    public static void main(String[] args) {
+        String cade = "Hola mundo!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+    }
+}
