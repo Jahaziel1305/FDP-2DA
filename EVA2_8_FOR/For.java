91795c9ae521989e306c6667942fce74501de516
Jahaziel <l25550366@chihuahua2.tecnm.mx>
2025-10-09 12:13:58 -0600

    #

---
+++ EVA2_8_FOR/For.java
@@ -0,0 +1,17 @@
+package EVA2_8_FOR;
+
+public class For {
+    public static void main(String[] args) {
+        // 3 parametros
+        // variable y valor de inicio
+        // hasta donde llegas
+        // incremento o decremento
+        // i++ significa sumarle un valor de la i
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hola mundo!");
+        }
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
